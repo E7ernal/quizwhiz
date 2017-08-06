5cf59a8cacf884ff6212c55a21c48779ab4f0ad8
Zachary Mott <zach.mott@gmail.com>
2017-08-05 21:25:48 -0500

    Initialize quizwhiz django application.

---
+++ quizwhiz/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for quizwhiz project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "quizwhiz.settings")
+
+application = get_wsgi_application()
