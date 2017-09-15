12d8715a0480df57e781a02022c3839ef02089b3
Zachary Mott <zach.mott@gmail.com>
2017-09-15 15:50:00 -0500

    Configure Django/filer/django-s3-storage to use cdn.quizwhiz.io for serving media.

--- quizwhiz/settings.py
+++ quizwhiz/settings.py
@@ -119,12 +119,11 @@ EMAIL_HOST = 'email-smtp.us-west-2.amazonaws.com'
 # EMAIL_HOST_PASSWORD = 'Define me in local_settings.py.'
 EMAIL_PORT = 25
 
-# +------------------------------------------------------------------------------------------------+
-# |                                                                                                |
-# |                                    django.contrib.messages                                     |
-# |                                                                                                |
-# +------------------------------------------------------------------------------------------------+
-# Change default message tags to things that bootstrap recognizes.
+# Media storage. This sends uploaded media files to AWS S3.
+DEFAULT_FILE_STORAGE = 'django_s3_storage.storage.S3Storage'
+
+# django.contrib.messages settings.
+# This changes default message tags to things that bootstrap recognizes.
 # https://docs.djangoproject.com/en/1.11/ref/contrib/messages/#message-tags
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
@@ -204,18 +203,8 @@ AWS_REGION = 'us-west-2'
 AWS_S3_ADDRESSING_STYLE = 'auto'
 AWS_S3_BUCKET_AUTH = False
 AWS_S3_BUCKET_NAME = 'quizwhiz.io'
-AWS_S3_CUSTOM_DOMAIN = "{bucket}.s3.amazonaws.com".format(bucket=AWS_S3_BUCKET_NAME)
+# AWS_S3_PUBLIC_URL = 'Define me in local_settings.py.'
 AWS_S3_GZIP = True
-
-# +------------------------------------------------------------------------------------------------+
-# |                                                                                                |
-# |                              django media storage settings                                     |
-# |                                                                                                |
-# +------------------------------------------------------------------------------------------------+
-
-# This comes after 'boto' because it depends on AWS_S3_CUSTOM_DOMAIN
-DEFAULT_FILE_STORAGE = 'django_s3_storage.storage.S3Storage'
-MEDIA_URL = "//{media_domain}/".format(media_domain=AWS_S3_CUSTOM_DOMAIN)
 
 # +------------------------------------------------------------------------------------------------+
 # |                                                                                                |
